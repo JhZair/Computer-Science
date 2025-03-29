dde8e6271cadf2f782f0625b6c30dafd5a7fe416
JhZair <98540197+JhZair@users.noreply.github.com>
2025-03-29 08:24:28 -0500

    Primer commit

---
+++ clase1.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+int main() 
+    //int a = 321;
+    //int * p = &a;
+    //char * r = (char *)p;
+    //*r = 'B';
+    //std::cout<< p <<' '<< r <<std::endl;
+	
+	int arr[8] = {2,4,6,8,1,3,5,7};
+	int * p = arr;
+	int * q = arr + 3;)
+	//fin = 
+	cout << arr[1]<<endl;
+	
+
+    return 0;
+}
+// investigar sobre offset p (q-p) * 2, intercambiar los elementos de un array con punteros, con un puntero a la mitad
