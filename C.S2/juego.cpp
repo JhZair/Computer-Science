247fb81a2c692e0a247c88d6c5b478cde334367f
JhZair <98540197+JhZair@users.noreply.github.com>
2025-04-12 08:18:21 -0500

    Add files via upload

---
+++ C.S2/juego.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+void iniciarJuego(){
+	int arr[3][3]= {{1,3,8},{4,6,9},{2,5,7}};
+	
+	cout << "Bienvenido al juego"<<endl;
+	
+	cout << "Tienes que ordenar esta matriz!!"<< endl;
+	
+	for(int i = 0; i < 3 ; i++){
+		for(int i = 0; i < 3 ; i++){
+			cout << 
+		}
+	}
+	
+}
+
+int main() {
+
+	iniciarJuego();
+	
+	return 0;
+}
+
+//ordenar array 3x3 que tenga un hueco que se mueva con las flechas 
+//evitar sumar un iterador a un puntero en un bucle, sino cambiar de lugar los punteros
+
+//tarea de multiplicar matrices función mult(int (*p)[3][3])???
+//matriz A=M[0] B=M[1], C=M[2], y m es un array tridimensional
